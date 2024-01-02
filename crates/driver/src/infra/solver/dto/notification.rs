snowmania96/Rust_Service

commit 7054721425d81d9e224bfa2a2b66236f23863c00

Update SolverEngine API docs (#2192)
# Description
Docs were slightly out of date

# Changes

- [x] Updates all fields and structs on the existing endpoint
- [x] Remove pre-interactions for jit orders (for some reason we didn't
populate post interactions and since those orders aren't really used in
practice anyways, it seemed simpler to remove pre interactions instead
of adding post interactions).
- [x] Flatten notification type to make it consistent with the way we
encode enums elsewhere in the new API (cf. `Liquidity`)
- [x] Flatten Score enum to make it consistent
- [x] Document notify endpoint

## How to test
Past into https://editor.swagger.io/ and compare line by line with the
rust structs

## Related Issues

Fixes #2182

crates/driver/src/infra/solver/dto/notification.rs
@@ -4,7 +4,7 @@ use {
             competition::{auction, solution},
             eth,
         },
-        infra::{notify, notify::SimulationSucceededAtLeastOnce},
+        infra::notify,
         util::serialize,
     },
     serde::Serialize,
@@ -25,41 +25,39 @@ impl Notification {
             kind: match kind {
                 notify::Kind::Timeout => Kind::Timeout,
                 notify::Kind::EmptySolution => Kind::EmptySolution,
-                notify::Kind::SimulationFailed(block, tx, simulated_once) => {
-                    Kind::SimulationFailed(
-                        block.0,
-                        Tx {
+                notify::Kind::SimulationFailed(block, tx, succeeded_once) => {
+                    Kind::SimulationFailed {
+                        block: block.0,
+                        tx: Tx {
                             from: tx.from.into(),
                             to: tx.to.into(),
                             input: tx.input.into(),
                             value: tx.value.into(),
                             access_list: tx.access_list.into(),
                         },
-                        simulated_once,
-                    )
+                        succeeded_once,
+                    }
                 }
-                notify::Kind::ScoringFailed(notify::ScoreKind::ZeroScore) => {
-                    Kind::ScoringFailed(ScoreKind::ZeroScore)
-                }
+                notify::Kind::ScoringFailed(notify::ScoreKind::ZeroScore) => Kind::ZeroScore,
                 notify::Kind::ScoringFailed(notify::ScoreKind::ScoreHigherThanQuality(
                     score,
                     quality,
-                )) => Kind::ScoringFailed(ScoreKind::ScoreHigherThanQuality {
+                )) => Kind::ScoreHigherThanQuality {
                     score: score.0.get(),
                     quality: quality.0,
-                }),
+                },
                 notify::Kind::ScoringFailed(notify::ScoreKind::SuccessProbabilityOutOfRange(
                     success_probability,
-                )) => Kind::ScoringFailed(ScoreKind::SuccessProbabilityOutOfRange {
+                )) => Kind::SuccessProbabilityOutOfRange {
                     probability: success_probability,
-                }),
+                },
                 notify::Kind::ScoringFailed(notify::ScoreKind::ObjectiveValueNonPositive(
                     quality,
                     gas_cost,
-                )) => Kind::ScoringFailed(ScoreKind::ObjectiveValueNonPositive {
+                )) => Kind::ObjectiveValueNonPositive {
                     quality: quality.0,
                     gas_cost: gas_cost.get().0,
-                }),
+                },
                 notify::Kind::NonBufferableTokensUsed(tokens) => Kind::NonBufferableTokensUsed {
                     tokens: tokens.into_iter().map(|token| token.0 .0).collect(),
                 },
@@ -69,16 +67,16 @@ impl Notification {
                     }
                 }
                 notify::Kind::DuplicatedSolutionId => Kind::DuplicatedSolutionId,
-                notify::Kind::Settled(kind) => Kind::Settled(match kind {
-                    notify::Settlement::Success(hash) => Settlement::Success {
+                notify::Kind::Settled(kind) => match kind {
+                    notify::Settlement::Success(hash) => Kind::Success {
                         transaction: hash.0,
                     },
-                    notify::Settlement::Revert(hash) => Settlement::Revert {
+                    notify::Settlement::Revert(hash) => Kind::Revert {
                         transaction: hash.0,
                     },
-                    notify::Settlement::SimulationRevert => Settlement::SimulationRevert,
-                    notify::Settlement::Fail => Settlement::Fail,
-                }),
+                    notify::Settlement::SimulationRevert => Kind::Cancelled,
+                    notify::Settlement::Fail => Kind::Fail,
+                },
             },
         }
     }
@@ -95,42 +93,17 @@ pub struct Notification {
 
 #[serde_as]
 #[derive(Debug, Serialize)]
-#[serde(rename_all = "lowercase")]
+#[serde(rename_all = "lowercase", tag = "kind")]
 pub enum Kind {
     Timeout,
     EmptySolution,
     DuplicatedSolutionId,
-    SimulationFailed(BlockNo, Tx, SimulationSucceededAtLeastOnce),
-    ScoringFailed(ScoreKind),
-    NonBufferableTokensUsed {
-        tokens: BTreeSet<eth::H160>,
+    #[serde(rename_all = "camelCase")]
+    SimulationFailed {
+        block: BlockNo,
+        tx: Tx,
+        succeeded_once: bool,
     },
-    SolverAccountInsufficientBalance {
-        #[serde_as(as = "serialize::U256")]
-        required: eth::U256,
-    },
-    Settled(Settlement),
-}
-
-type BlockNo = u64;
-
-#[serde_as]
-#[derive(Debug, Serialize)]
-#[serde(rename_all = "camelCase")]
-pub struct Tx {
-    pub from: eth::H160,
-    pub to: eth::H160,
-    #[serde_as(as = "serialize::Hex")]
-    pub input: Vec<u8>,
-    #[serde_as(as = "serialize::U256")]
-    pub value: eth::U256,
-    pub access_list: AccessList,
-}
-
-#[serde_as]
-#[derive(Debug, Serialize)]
-#[serde(rename_all = "lowercase")]
-pub enum ScoreKind {
     ZeroScore,
     ScoreHigherThanQuality {
         #[serde_as(as = "serialize::U256")]
@@ -148,14 +121,34 @@ pub enum ScoreKind {
         #[serde_as(as = "serialize::U256")]
         gas_cost: eth::U256,
     },
+    NonBufferableTokensUsed {
+        tokens: BTreeSet<eth::H160>,
+    },
+    SolverAccountInsufficientBalance {
+        #[serde_as(as = "serialize::U256")]
+        required: eth::U256,
+    },
+    Success {
+        transaction: eth::H256,
+    },
+    Revert {
+        transaction: eth::H256,
+    },
+    Cancelled,
+    Fail,
 }
+
+type BlockNo = u64;
 
 #[serde_as]
 #[derive(Debug, Serialize)]
-#[serde(rename_all = "lowercase")]
-pub enum Settlement {
-    Success { transaction: eth::H256 },
-    Revert { transaction: eth::H256 },
-    SimulationRevert,
-    Fail,
+#[serde(rename_all = "camelCase")]
+pub struct Tx {
+    pub from: eth::H160,
+    pub to: eth::H160,
+    #[serde_as(as = "serialize::Hex")]
+    pub input: Vec<u8>,
+    #[serde_as(as = "serialize::U256")]
+    pub value: eth::U256,
+    pub access_list: AccessList,
 }

crates/solvers/src/api/routes/notify/dto/notification.rs
@@ -3,7 +3,7 @@ use {
         domain::{
             auction,
             eth,
-            notification::{self, SimulationSucceededAtLeastOnce},
+            notification::{self},
         },
         util::serialize,
     },
@@ -26,20 +26,22 @@ impl Notification {
             kind: match &self.kind {
                 Kind::Timeout => notification::Kind::Timeout,
                 Kind::EmptySolution => notification::Kind::EmptySolution,
-                Kind::SimulationFailed(block, tx, succeeded_at_least_once) => {
-                    notification::Kind::SimulationFailed(
-                        *block,
-                        eth::Tx {
-                            from: tx.from.into(),
-                            to: tx.to.into(),
-                            input: tx.input.clone().into(),
-                            value: tx.value.into(),
-                            access_list: tx.access_list.clone(),
-                        },
-                        *succeeded_at_least_once,
-                    )
-                }
-                Kind::ScoringFailed(ScoreKind::ObjectiveValueNonPositive { quality, gas_cost }) => {
+                Kind::SimulationFailed {
+                    block,
+                    tx,
+                    succeeded_once,
+                } => notification::Kind::SimulationFailed(
+                    *block,
+                    eth::Tx {
+                        from: tx.from.into(),
+                        to: tx.to.into(),
+                        input: tx.input.clone().into(),
+                        value: tx.value.into(),
+                        access_list: tx.access_list.clone(),
+                    },
+                    *succeeded_once,
+                ),
+                Kind::ObjectiveValueNonPositive { quality, gas_cost } => {
                     notification::Kind::ScoringFailed(
                         notification::ScoreKind::ObjectiveValueNonPositive(
                             (*quality).into(),
@@ -47,10 +49,10 @@ impl Notification {
                         ),
                     )
                 }
-                Kind::ScoringFailed(ScoreKind::ZeroScore) => {
+                Kind::ZeroScore => {
                     notification::Kind::ScoringFailed(notification::ScoreKind::ZeroScore)
                 }
-                Kind::ScoringFailed(ScoreKind::ScoreHigherThanQuality { score, quality }) => {
+                Kind::ScoreHigherThanQuality { score, quality } => {
                     notification::Kind::ScoringFailed(
                         notification::ScoreKind::ScoreHigherThanQuality(
                             (*score).into(),
@@ -58,7 +60,7 @@ impl Notification {
                         ),
                     )
                 }
-                Kind::ScoringFailed(ScoreKind::SuccessProbabilityOutOfRange { probability }) => {
+                Kind::SuccessProbabilityOutOfRange { probability } => {
                     notification::Kind::ScoringFailed(
                         notification::ScoreKind::SuccessProbabilityOutOfRange(
                             (*probability).into(),
@@ -78,16 +80,16 @@ impl Notification {
                     notification::Kind::SolverAccountInsufficientBalance(eth::Ether(*required))
                 }
                 Kind::DuplicatedSolutionId => notification::Kind::DuplicatedSolutionId,
-                Kind::Settled(kind) => notification::Kind::Settled(match kind {
-                    Settlement::Success { transaction } => {
-                        notification::Settlement::Success(*transaction)
-                    }
-                    Settlement::Revert { transaction } => {
-                        notification::Settlement::Revert(*transaction)
-                    }
-                    Settlement::SimulationRevert => notification::Settlement::SimulationRevert,
-                    Settlement::Fail => notification::Settlement::Fail,
-                }),
+                Kind::Success { transaction } => {
+                    notification::Kind::Settled(notification::Settlement::Success(*transaction))
+                }
+                Kind::Revert { transaction } => {
+                    notification::Kind::Settled(notification::Settlement::Revert(*transaction))
+                }
+                Kind::Cancelled => {
+                    notification::Kind::Settled(notification::Settlement::SimulationRevert)
+                }
+                Kind::Fail => notification::Kind::Settled(notification::Settlement::Fail),
             },
         }
     }
@@ -100,47 +102,23 @@ pub struct Notification {
     #[serde_as(as = "Option<DisplayFromStr>")]
     auction_id: Option<i64>,
     solution_id: Option<u64>,
+    #[serde(flatten)]
     kind: Kind,
 }
 
 #[serde_as]
 #[derive(Debug, Deserialize)]
-#[serde(rename_all = "lowercase")]
+#[serde(rename_all = "lowercase", tag = "kind")]
 pub enum Kind {
     Timeout,
     EmptySolution,
     DuplicatedSolutionId,
-    SimulationFailed(BlockNo, Tx, SimulationSucceededAtLeastOnce),
-    ScoringFailed(ScoreKind),
-    NonBufferableTokensUsed {
-        tokens: BTreeSet<H160>,
+    #[serde(rename_all = "camelCase")]
+    SimulationFailed {
+        block: BlockNo,
+        tx: Tx,
+        succeeded_once: bool,
     },
-    SolverAccountInsufficientBalance {
-        #[serde_as(as = "serialize::U256")]
-        required: U256,
-    },
-    Settled(Settlement),
-}
-
-type BlockNo = u64;
-
-#[serde_as]
-#[derive(Debug, Deserialize)]
-#[serde(rename_all = "camelCase", deny_unknown_fields)]
-pub struct Tx {
-    from: H160,
-    to: H160,
-    #[serde_as(as = "serialize::Hex")]
-    input: Vec<u8>,
-    #[serde_as(as = "serialize::U256")]
-    value: U256,
-    access_list: AccessList,
-}
-
-#[serde_as]
-#[derive(Debug, Deserialize)]
-#[serde(rename_all = "lowercase")]
-pub enum ScoreKind {
     ZeroScore,
     ScoreHigherThanQuality {
         #[serde_as(as = "serialize::U256")]
@@ -158,14 +136,34 @@ pub enum ScoreKind {
         #[serde_as(as = "serialize::U256")]
         gas_cost: U256,
     },
+    NonBufferableTokensUsed {
+        tokens: BTreeSet<H160>,
+    },
+    SolverAccountInsufficientBalance {
+        #[serde_as(as = "serialize::U256")]
+        required: U256,
+    },
+    Success {
+        transaction: H256,
+    },
+    Revert {
+        transaction: H256,
+    },
+    Cancelled,
+    Fail,
 }
+
+type BlockNo = u64;
 
 #[serde_as]
 #[derive(Debug, Deserialize)]
-#[serde(rename_all = "lowercase")]
-pub enum Settlement {
-    Success { transaction: H256 },
-    Revert { transaction: H256 },
-    SimulationRevert,
-    Fail,
+#[serde(rename_all = "camelCase", deny_unknown_fields)]
+pub struct Tx {
+    from: H160,
+    to: H160,
+    #[serde_as(as = "serialize::Hex")]
+    input: Vec<u8>,
+    #[serde_as(as = "serialize::U256")]
+    value: U256,
+    access_list: AccessList,
 }

crates/driver/src/tests/setup/mod.rs
@@ -56,18 +56,21 @@ pub enum Partial {
 
 #[serde_as]
 #[derive(Debug, Clone, serde::Serialize)]
-#[serde(rename_all = "lowercase")]
+#[serde(rename_all = "lowercase", tag = "kind")]
 pub enum Score {
     Solver {
         #[serde_as(as = "serialize::U256")]
         score: eth::U256,
     },
-    RiskAdjusted(f64),
+    #[serde(rename_all = "camelCase")]
+    RiskAdjusted { success_probability: f64 },
 }
 
 impl Default for Score {
     fn default() -> Self {
-        Self::RiskAdjusted(1.0)
+        Self::RiskAdjusted {
+            success_probability: 1.0,
+        }
     }
 }
 

crates/driver/src/infra/solver/dto/solution.rs
@@ -200,9 +200,9 @@ impl Solutions {
                         Score::Solver { score } => {
                             competition::solution::SolverScore::Solver(score)
                         }
-                        Score::RiskAdjusted(success_probability) => {
-                            competition::solution::SolverScore::RiskAdjusted(success_probability)
-                        }
+                        Score::RiskAdjusted {
+                            success_probability,
+                        } => competition::solution::SolverScore::RiskAdjusted(success_probability),
                     },
                     weth,
                 )
@@ -377,11 +377,12 @@ enum SigningScheme {
 
 #[serde_as]
 #[derive(Debug, Deserialize)]
-#[serde(rename_all = "lowercase", deny_unknown_fields)]
+#[serde(rename_all = "lowercase", deny_unknown_fields, tag = "kind")]
 pub enum Score {
     Solver {
         #[serde_as(as = "serialize::U256")]
         score: eth::U256,
     },
-    RiskAdjusted(f64),
+    #[serde(rename_all = "camelCase")]
+    RiskAdjusted { success_probability: f64 },
 }

crates/driver/src/tests/cases/score_competition.rs
@@ -12,7 +12,7 @@ async fn solver_score_winner() {
         .pool(ab_pool())
         .order(ab_order())
         .solution(ab_solution().score(Score::Solver { score: 2902421280589416499u128.into()})) // higher than objective value
-        .solution(ab_solution().score(Score::RiskAdjusted(0.4)))
+        .solution(ab_solution().score(Score::RiskAdjusted{ success_probability: 0.4}))
         .done()
         .await;
 
@@ -31,7 +31,9 @@ async fn risk_adjusted_score_winner() {
         .solution(ab_solution().score(Score::Solver {
             score: DEFAULT_SCORE_MIN.into(),
         }))
-        .solution(ab_solution().score(Score::RiskAdjusted(0.9)))
+        .solution(ab_solution().score(Score::RiskAdjusted {
+            success_probability: 0.9,
+        }))
         .done()
         .await;
 

crates/shared/src/http_solver/model.rs
@@ -7,7 +7,7 @@ use {
     ethcontract::{Bytes, H160},
     model::{
         auction::AuctionId,
-        order::{Interactions, OrderData, OrderUid},
+        order::{OrderData, OrderUid},
         ratio_as_decimal,
         signature::Signature,
     },
@@ -347,8 +347,6 @@ pub struct NativeLiquidityOrder {
     pub data: OrderData,
     #[serde(flatten)]
     pub signature: Signature,
-    #[serde(default)]
-    pub interactions: Interactions,
 }
 
 #[serde_as]
@@ -1166,7 +1164,6 @@ mod tests {
                         ..Default::default()
                     },
                     signature: Signature::Eip1271(vec![1, 2, 3, 4]),
-                    interactions: Default::default(),
                 },
                 exec_sell_amount: 50.into(),
                 exec_buy_amount: 51.into(),

crates/solver/src/solver/http_solver/settlement.rs
@@ -338,7 +338,7 @@ fn convert_foreign_liquidity_orders(
                 },
                 data: liquidity.order.data,
                 signature: liquidity.order.signature,
-                interactions: liquidity.order.interactions,
+                interactions: Default::default(),
             };
             let converted = order_converter.normalize_limit_order(BalancedOrder::full(order))?;
             Ok(ExecutedLimitOrder {
@@ -609,7 +609,6 @@ mod tests {
                     ..Default::default()
                 },
                 signature: Signature::PreSign,
-                interactions: Default::default(),
             },
             exec_sell_amount: 101.into(),
             exec_buy_amount: 102.into(),

crates/solvers/openapi.yml
@@ -23,13 +23,43 @@ paths:
           content:
             application/json:
               schema:
-                $ref: "#/components/schemas/Solution"
+                type: array
+                items:
+                  $ref: "#/components/schemas/Solution"
         400:
           description: There is something wrong with the request.
         429:
           description: The solver cannot keep up. It is too busy to handle more requests.
         500:
           description: Something went wrong when handling the request.
+  /notify:
+    post:
+      description: |
+        Receive a status notification about a previously provided solution
+      requestBody:
+        required: true
+        content:
+          application/json:
+            schema:
+              type: object
+              properties:
+                auctionId:
+                  description: |
+                    The auction ID of the auction that the solution was provided
+                    for.
+                  type: number
+                solutionId:
+                  description: |
+                    The solution ID within the auction for which the notification applies
+                  type: number
+                kind:
+                  description: |
+                    The kind of notification.
+                  type: string
+                  enum: [Timeout, EmptySolution, DuplicatedSolutionId, SimulationFailed, ZeroScore, ScoreHigherThanQuality, SuccessProbabilityOutOfRange, ObjectiveValueNonPositive, NonBufferableTokensUsed, SolverAccountInsufficientBalance, Success, Revert, Cancelled, Failed]
+      responses:
+        200:
+          description: notification successfully received.
 
 components:
   schemas:
@@ -282,6 +312,9 @@ components:
                     $ref: "#/components/schemas/Decimal"
         fee:
           $ref: "#/components/schemas/Decimal"
+        version:
+          type: string
+          enum: ["v0", "v3plus"]
 
     StablePool:
       description: |
@@ -535,6 +568,17 @@ components:
             must be included in the auction input.
           allOf:
             - $ref: "#/components/schemas/OrderUid"
+        fee:
+          description: |
+            The sell token amount that should be taken as a fee for this
+            trade. This only gets returned for partially fillable limit
+            orders and only refers to the actual amount filled by the trade.
+        executedAmount:
+          description: |
+            The amount of the order that was executed. This is denoted in
+            "sellToken" for sell orders, and "buyToken" for buy orders.
+          allOf:
+            - $ref: "#/components/schemas/TokenAmount"
 
     JitTrade:
       description: |
@@ -546,34 +590,23 @@ components:
         kind:
           type: string
           enum: [jit]
+        executedAmount:
+          description: |
+            The amount of the order that was executed. This is denoted in
+            "sellToken" for sell orders, and "buyToken" for buy orders.
+          allOf:
+            - $ref: "#/components/schemas/TokenAmount"
         order:
           description: |
             The just-in-time liquidity order to execute in a solution.
           allOf:
             - $ref: "#/components/schemas/JitOrder"
-
     Trade:
       description: |
         A trade for a CoW Protocol order included in a solution.
-      allOf:
-        - oneOf:
-            - $ref: "#/components/schemas/Fulfillment"
-            - $ref: "#/components/schemas/JitTrade"
-        - type: object
-          required:
-            - executedAmount
-          properties:
-            executedAmount:
-              description: |
-                The amount of the order that was executed. This is denoted in
-                "sellToken" for sell orders, and "buyToken" for buy orders.
-              allOf:
-                - $ref: "#/components/schemas/TokenAmount"
-            fee:
-              description: |
-                The sell token amount that should be taken as a fee for this
-                trade. This only gets returned for partially fillable limit
-                orders and only refers to the actual amount filled by the trade.
+      oneOf:
+        - $ref: "#/components/schemas/Fulfillment"
+        - $ref: "#/components/schemas/JitTrade"
 
     LiquidityInteraction:
       description: |
@@ -630,7 +663,7 @@ components:
         - kind
         - target
         - value
-        - callData
+        - calldata
         - inputs
         - outputs
       properties:
@@ -641,7 +674,7 @@ components:
           $ref: "#/components/schemas/Address"
         value:
           $ref: "#/components/schemas/TokenAmount"
-        callData:
+        calldata:
           description: |
             The EVM calldata bytes.
           type: string
@@ -681,10 +714,15 @@ components:
         The computed solution for a given auction.
       type: object
       required:
+        - id
         - prices
         - trades
         - interactions
+        - score
       properties:
+        id:
+          description: An opaque identifier for the solution.
+          type: number
         prices:
           description: |
             A clearing price map of token address to price. The price can have
@@ -704,3 +742,30 @@ components:
           type: array
           items:
             $ref: "#/components/schemas/Interaction"
+        score:
+          description: |
+            Information required by the driver to score the solution. 
+            
+            Can either be a raw score or a success probability.
+          oneOf:
+            - type: object
+              properties:
+                kind: 
+                  type: string
+                  enum: [solver]
+                score:
+                  description: |
+                    The raw score of the solution. This is the score that is
+                    used by the driver to rank solutions.
+                  allOf:
+                    - $ref: "#/components/schemas/Decimal"
+            - type: object
+              properties:
+                kind: 
+                  type: string
+                  enum: [riskadjusted]
+                successProbability: 
+                  description: |
+                    The revert probability of the solution. Used by the driver to compute a risk-adjusted score.
+                  type: number
+                  example: 0.9

commit 65bdeb15f6ab7e0b5bc5221ccab47b8c7e0a4ffc

Fix notify dto (#2227)
# Description
Notification calls from the driver to solver engines are currently
failing due to:

> Failed to deserialize the JSON body into the target type: invalid
type: map, expected variant identifier at line 1 column 143

I broke this with #2192 where I missed adding the flatten directive on
the driver dto.

# Changes
- [x] Fix dto on both sides (we can no longer deny_unknow_fields due to
this [serde bug](https://github.com/serde-rs/serde/issues/1600))
- [x] Warn if the notify call is not successful

## How to test
Run e.g. the partial fill colocation test case. See warning before
adjusting the dto and now no longer see it.

crates/driver/src/infra/solver/dto/notification.rs
@@ -89,6 +89,7 @@ impl Notification {
 pub struct Notification {
     auction_id: Option<String>,
     solution_id: Option<u64>,
+    #[serde(flatten)]
     kind: Kind,
 }
 

crates/driver/src/infra/solver/mod.rs
@@ -194,7 +194,9 @@ impl Solver {
             req = req.header("X-REQUEST-ID", id);
         }
         let future = async move {
-            let _ = util::http::send(SOLVER_RESPONSE_MAX_BYTES, req).await;
+            if let Err(error) = util::http::send(SOLVER_RESPONSE_MAX_BYTES, req).await {
+                tracing::warn!(?error, "failed to notify solver");
+            }
         };
         tokio::task::spawn(future.in_current_span());
     }

crates/driver/src/util/http.rs
@@ -9,7 +9,15 @@ pub async fn send(limit_bytes: usize, req: reqwest::RequestBuilder) -> Result<St
         }
         data.extend_from_slice(&chunk);
     }
-    String::from_utf8(data).map_err(Into::into)
+    let body = String::from_utf8(data).map_err(Error::NotUtf8)?;
+    if res.status().is_success() {
+        Ok(body)
+    } else {
+        Err(Error::NotOk {
+            code: res.status().as_u16(),
+            body: body.clone(),
+        })
+    }
 }
 
 #[derive(Debug, Error)]
@@ -20,4 +28,6 @@ pub enum Error {
     ResponseTooLarge { limit_bytes: usize },
     #[error("the response could not be parsed as UTF-8: {0:?}")]
     NotUtf8(#[from] std::string::FromUtf8Error),
+    #[error("the response status was not 2xx but {code:?}, body: {body:?}")]
+    NotOk { code: u16, body: String },
 }

crates/solvers/Cargo.toml
@@ -36,7 +36,7 @@ thiserror = "1"
 tokio = { version = "1", features = ["macros", "rt-multi-thread", "signal", "time"] }
 toml = "0.7"
 tower = "0.4"
-tower-http = { version = "0.4", features = ["trace"] }
+tower-http = { version = "0.4", features = ["limit", "trace"] }
 web3 = "0.19"
 
 # TODO Once solvers are ported and E2E tests set up, slowly migrate code and

crates/solvers/src/api/routes/notify/dto/notification.rs
@@ -98,7 +98,7 @@ impl Notification {
 
 #[serde_as]
 #[derive(Debug, Deserialize)]
-#[serde(rename_all = "camelCase", deny_unknown_fields)]
+#[serde(rename_all = "camelCase")]
 pub struct Notification {
     #[serde_as(as = "Option<DisplayFromStr>")]
     auction_id: Option<i64>,